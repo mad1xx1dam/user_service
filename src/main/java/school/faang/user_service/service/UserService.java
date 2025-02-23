1992abbada8040e28e5f5041af4c2aec58228367
VladimirGontarenko <mister.gontarenko@yandex.ru>
2025-02-23 17:36:23 +0400

    feature/BJS2-52486-user-profile-traffic-analysis-publisher

--- src/main/java/school/faang/user_service/service/UserService.java
+++ src/main/java/school/faang/user_service/service/UserService.java
@@ -3,8 +3,11 @@ package school.faang.user_service.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import school.faang.user_service.entity.User;
 import school.faang.user_service.repository.UserRepository;
+import school.faang.user_service.utility.aspect_annotations.UserProfileViewed;
+
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -14,11 +17,14 @@ import java.util.NoSuchElementException;
 public class UserService {
     private final UserRepository userRepository;
 
+    @UserProfileViewed
+    @Transactional
     public User getUser(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException(String.format("User with id #%d not found", id)));
     }
 
+    @Transactional
     public List<User> getUsers(List<Long> ids) {
         log.info("Getting Users with ids {}", ids);
         return userRepository.findAllById(ids);
@@ -28,11 +34,14 @@ public class UserService {
         return userRepository.existsById(id);
     }
 
+    @Transactional
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
+    @Transactional
     public List<User> findAllUsers() {
+
         return userRepository.findAll();
     }
 }
---
+++ src/main/java/school/faang/user_service/utility/aspect_annotations/UserProfileViewed.java
@@ -0,0 +1,4 @@
+package school.faang.user_service.utility.aspect_annotations;
+
+public @interface UserProfileViewed {
+}
